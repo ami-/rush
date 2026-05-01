9340e23e8eb1ba1c2ee81cc9b44b5cdb01e26e56
Adi <amihalcea@ledgertech.com>
2026-05-01 11:15:41 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::env;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
 
 const BUILTINS: [&str; 3] = ["echo", "exit", "type"];
 
@@ -14,14 +16,7 @@ fn main() {
         let (cmd, args) = parse_command(line.trim());
 
         match cmd {
-            "type" => {
-                let (cmd, _) = parse_command(args);
-                if BUILTINS.contains(&cmd) {
-                    println!("{} is a shell builtin", cmd);
-                } else {
-                    println!("{}: not found", cmd);
-                }
-            }
+            "type" => do_type(args),
             "echo" => println!("{}", args),
             "exit" => break,
             _ => println!("{}: command not found", cmd),
@@ -33,4 +28,24 @@ fn parse_command(line: &str) -> (&str, &str) {
     let first_word = line.split_whitespace().next().unwrap_or("");
     let rest = line[first_word.len()..].trim();
     (first_word, rest)
+}
+
+fn do_type(line: &str) {
+    let (cmd, _) = parse_command(line);
+    if BUILTINS.contains(&cmd) {
+        println!("{} is a shell builtin", cmd);
+        return;
+    }
+    if let Ok(path) = env::var("PATH") {
+        for dir in env::split_paths(&path) {
+            let full_path = dir.join(cmd);
+            if let Ok(meta) = full_path.metadata() {
+                if meta.permissions().mode() & 0o111 != 0 {
+                    println!("{} is {}", cmd, full_path.display());
+                    return;
+                }
+            }
+        }
+    }
+    println!("{}: not found", cmd);
 }
