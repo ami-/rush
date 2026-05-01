c23427c8c1aa2a35ff20fd68a5a951d3f32e4e14
Adi <amihalcea@ledgertech.com>
2026-05-01 12:03:31 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
+use std::path;
+use std::process::Command;
 
 const BUILTINS: [&str; 3] = ["echo", "exit", "type"];
 
@@ -19,6 +21,13 @@ fn main() {
             "type" => do_type(args),
             "echo" => println!("{}", args),
             "exit" => break,
+            _ if let Some(exe_path) = find_executable(cmd) => {
+                //let exe_path = dbg!(exe_path);
+                //println!("{}", exe_path.display());
+                let arg_i = args.split_whitespace();
+                let exe = exe_path.file_name().expect("bad file name");
+                let _ = Command::new(exe).args(arg_i).status();
+            }
             _ => println!("{}: command not found", cmd),
         }
     }
@@ -36,16 +45,24 @@ fn do_type(line: &str) {
         println!("{} is a shell builtin", cmd);
         return;
     }
+    match find_executable(cmd) {
+        Some(full_path) => println!("{} is {}", cmd, full_path.display()),
+        None => println!("{}: not found", cmd),
+    };
+}
+
+fn find_executable(name: &str) -> Option<path::PathBuf> {
     if let Ok(path) = env::var("PATH") {
         for dir in env::split_paths(&path) {
-            let full_path = dir.join(cmd);
-            if let Ok(meta) = full_path.metadata() {
+            let full_path = dir.join(name);
+            if full_path.is_file()
+                && let Ok(meta) = full_path.metadata()
+            {
                 if meta.permissions().mode() & 0o111 != 0 {
-                    println!("{} is {}", cmd, full_path.display());
-                    return;
+                    return Some(full_path);
                 }
             }
         }
     }
-    println!("{}: not found", cmd);
+    None
 }
